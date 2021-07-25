2f0243f59e33b21def1e58316c883a985a91ad1e
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-07-26 00:06:27 +0530

    feat: set default type any for SettingsSchema

--- src/set.ts
+++ src/set.ts
@@ -2,7 +2,7 @@ import { getSettings } from './get-settings';
 import { saveSettings } from './save-settings';
 
 export async function set
-<SettingsSchema extends {}, K extends keyof SettingsSchema = keyof SettingsSchema>
+<SettingsSchema extends {} = any, K extends keyof SettingsSchema = keyof SettingsSchema>
 (key: K, value: SettingsSchema[K]): Promise<SettingsSchema>
 {
   const settings = await getSettings<SettingsSchema>();
