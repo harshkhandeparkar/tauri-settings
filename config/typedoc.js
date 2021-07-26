89d49aa7a066959f4013ca3bceb050456a0a4a0d
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-07-26 11:47:35 +0530

    feat: set up typedoc

---
+++ config/typedoc.js
@@ -0,0 +1,27 @@
+const { readdirSync, statSync } = require('fs');
+const { join } = require('path');
+
+const getListOfFiles = (dirPath) => {
+  const list = [];
+
+  readdirSync(dirPath).map((name) => {
+    if (statSync(join(dirPath, name)).isDirectory()) {
+      return list.push(...getListOfFiles(join(dirPath, name)));
+    }
+    else list.push(join(dirPath, name));
+  })
+
+  return list;
+}
+
+module.exports = {
+  excludeProtected: true,
+  excludePrivate: true,
+  excludeInternal: true,
+  includeVersion: true,
+  name: 'Tauri Settings',
+  entryPoints: getListOfFiles(join(__dirname, '..', 'src')),
+  out: [join(__dirname, '..', 'docs')],
+  readme: 'none',
+  theme: 'minimal'
+}
