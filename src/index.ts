105ac3eb63d48ecb742eed208841828d7f658477
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-07-27 01:35:47 +0530

    feat: export constants and config types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { has, get, set } from './settings//getter-setter';
 export { getSettings as getAll } from './fs/load-save';
 export { SettingsManager } from './settings-manager/settings-manager';
+
+export * as CONSTANTS from './constants';
+export { IConfig, ConfigOptions } from './config/config';
