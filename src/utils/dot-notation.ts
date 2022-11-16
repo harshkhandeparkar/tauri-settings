465b51c23e18c0ade41969bcaa040fbfdf25181d
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2022-11-16 10:58:33 +0530

    feat: <dot-notation.ts> * return null instead of undefined in getDotNotation * create keys if they do not exist in setDotNotation

--- src/utils/dot-notation.ts
+++ src/utils/dot-notation.ts
@@ -3,46 +3,52 @@ import type { Path, PathValue } from '../types/dot-notation';
 /**
  * @internal
  */
-export function getDotNotation<SettingsSchema, K extends Path<SettingsSchema>>(obj: SettingsSchema, path: K): PathValue<SettingsSchema, K> | undefined {
+export function getDotNotation<SettingsSchema, K extends Path<SettingsSchema> = Path<SettingsSchema>>(
+    settings: SettingsSchema,
+    path: K
+): PathValue<SettingsSchema, K> | null {
     if (typeof path !== 'string') throw 'Error: path must be a string';
 
     const keys = path.split('.');
 
-    let current: any = obj;
+    let current: PathValue<SettingsSchema, K> | SettingsSchema = settings;
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
 
         if (current[key] === undefined) {
-            return undefined;
+            return null;
         } else {
             current = current[key];
         }
     }
 
-    return current;
+    return current as PathValue<SettingsSchema, K>;
 }
 
 /**
  * @internal
  */
-export function setDotNotation<SettingsSchema, K extends Path<SettingsSchema>>(obj: SettingsSchema, path: K, value: PathValue<SettingsSchema, K>): PathValue<SettingsSchema, K> | undefined {
+export function setDotNotation<SettingsSchema, K extends Path<SettingsSchema> = Path<SettingsSchema>>(
+    settings: SettingsSchema,
+    path: K,
+    value: PathValue<SettingsSchema, K>
+): SettingsSchema {
     if (typeof path !== 'string') throw 'Error: path must be a string';
 
     const keys = path.split('.');
 
-    let current = obj;
+    let current: PathValue<SettingsSchema, K> | SettingsSchema = settings;
     for (let i = 0; i < keys.length - 1; i++) {
         const key = keys[i];
 
         if (current[key] === undefined) {
-            return undefined;
+            // if a key does not exist, create it
+            current[key] = {};
         }
 
         current = current[key];
     }
 
-    if (current[keys[keys.length - 1]] === undefined) return undefined;
-
     current[keys[keys.length - 1]] = value;
-    return value;
+    return settings;
 }
