74cd48a83a14b9415efcc08ce9ee80f9b09e9e9f
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-10 22:05:55 +0530

    docs: update example

--- examples/tauri-app/src/lib/Greet.svelte
+++ examples/tauri-app/src/lib/Greet.svelte
@@ -1,22 +1,29 @@
 <script>
   import { invoke } from "@tauri-apps/api/tauri"
 
-  let name = "";
-  let greetMsg = ""
+  let greetMsg = "loading name...";
 
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+  function change_name() {
+    invoke('plugin:settings|get', { key: 'name' }).then(console.log).catch(console.log)
   }
+
+  let new_name = "";
+  function set_name() {
+    invoke('plugin:settings|set', { key: 'name', value: new_name }).then(console.log).catch(console.log)
+  }
+
+  change_name()
+  console.log('testaddo')
 </script>
 
 <div>
+  <p>{greetMsg}</p>
+
   <div class="row">
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button on:click={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={new_name} />
+    <button on:click={set_name}>
       Greet
     </button>
   </div>
-  <p>{greetMsg}</p>
 </div>
 
