3d013bff2750e8882df89d495c60d96a63bd4472
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-22 17:01:49 +0530

    feat: added config as an option to init

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ mod config;
 mod fs;
 mod handlers;
 
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
+pub fn init<R: Runtime>(config: Option<Config>) -> TauriPlugin<R> {
 	Builder::new("settings")
 		.invoke_handler(tauri::generate_handler![handlers::has, handlers::get, handlers::set])
 		.setup(|app| {
-			let plugin_state = Config::new(&app.config(), None, None, None, None)?;
+			let plugin_state = config.unwrap_or(Config::new(&app.config(), None, None, None, None)?);
 
 			app.manage(plugin_state);
 			Ok(())
