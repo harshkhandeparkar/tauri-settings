544a1ea84763af6b2ba1344d9d4512ed599859d3
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-22 16:42:59 +0530

    feat: exported has, get, set in webview-src

--- webview-src/index.ts
+++ webview-src/index.ts
@@ -1,5 +1,21 @@
 import { invoke } from '@tauri-apps/api/tauri'
 
-export async function execute() {
-  await invoke('plugin:settings|execute')
+export async function has(
+  key: string | number | symbol
+): Promise<boolean> {
+  return await invoke('plugin:settings|has', { key })
+}
+
+export async function get<
+  SettingsSchema extends {} = any,
+  K extends keyof SettingsSchema = keyof SettingsSchema
+>(key: K): Promise<SettingsSchema[K]> {
+  return await invoke('plugin:settings|get', { key })
+}
+
+export async function set<
+  SettingsSchema extends {} = any,
+  K extends keyof SettingsSchema = keyof SettingsSchema
+>(key: K, value: SettingsSchema[K]): Promise<SettingsSchema[K]> {
+  return await invoke('plugin:settings|set', { key, value })
 }
