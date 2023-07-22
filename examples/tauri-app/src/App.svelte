d55e63c2a2cc6385871608ceaa3661bbefaedf0e
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-22 16:55:19 +0530

    feat: updated example

--- examples/tauri-app/src/App.svelte
+++ examples/tauri-app/src/App.svelte
@@ -3,12 +3,6 @@
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
   <div class="row">
     <Greet />
   </div>
