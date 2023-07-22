6aca4e580e760e47ad25258f1ad0fc5e75ece8bb
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-22 16:33:19 +0530

    feat: added has command

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,6 +4,25 @@ use tauri::{AppHandle, Runtime, State};
 use crate::{config::Config, fs::{load_settings_json, save_settings_json}};
 
 #[tauri::command]
+pub fn has<R: Runtime>(
+	_app: AppHandle<R>,
+	state: State<'_, Config>,
+	key: &str
+) -> Result<bool, String> {
+	let config = state.inner();
+	let settings_json = load_settings_json(config).map_err(|err| err.to_string())?;
+
+	let settings: Value = serde_json::from_str(&settings_json).map_err(|err| err.to_string())?;
+
+	let value = settings[key].clone();
+
+	match value {
+		Value::Null => Ok(false),
+		_ => Ok(true),
+	}
+}
+
+#[tauri::command]
 pub fn get<R: Runtime>(
 	_app: AppHandle<R>,
 	state: State<'_, Config>,
