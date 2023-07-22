1eebb20a96d632c039445b900c439a1df1bc3c36
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-22 20:54:50 +0530

    feat: added dot notation types to the api

--- webview-src/index.ts
+++ webview-src/index.ts
@@ -1,21 +1,27 @@
 import { invoke } from '@tauri-apps/api/tauri'
+import { Path, PathValue } from './types/dot-notation';
 
-export async function has(
-  key: string | number | symbol
-): Promise<boolean> {
+export async function has<
+  SettingsSchema,
+  K extends Path<SettingsSchema> = Path<SettingsSchema>
+> (key: K): Promise<boolean>
+{
   return await invoke('plugin:settings|has', { key })
 }
 
 export async function get<
-  SettingsSchema extends {} = any,
-  K extends keyof SettingsSchema = keyof SettingsSchema
->(key: K): Promise<SettingsSchema[K]> {
+  SettingsSchema,
+  K extends Path<SettingsSchema> = Path<SettingsSchema>
+> (key: K): Promise<PathValue<SettingsSchema, K>>
+{
   return await invoke('plugin:settings|get', { key })
 }
 
 export async function set<
-  SettingsSchema extends {} = any,
-  K extends keyof SettingsSchema = keyof SettingsSchema
->(key: K, value: SettingsSchema[K]): Promise<SettingsSchema[K]> {
+  SettingsSchema,
+  K extends Path<SettingsSchema> = Path<SettingsSchema>,
+  V extends PathValue<SettingsSchema, K> = PathValue<SettingsSchema, K>
+> (key: K, value: V): Promise<SettingsSchema>
+{
   return await invoke('plugin:settings|set', { key, value })
 }
