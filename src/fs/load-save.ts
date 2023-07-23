8e018256dec447e187bf7ebb91d5b83748e8d91a
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-24 01:01:17 +0530

    feat: completed implementation of SettingsManager with usePlugin

--- src/fs/load-save.ts
+++ src/fs/load-save.ts
@@ -1,19 +1,25 @@
 import { ensureSettingsFile, STATUS } from './ensure-settings-file';
 import { writeFile } from '@tauri-apps/api/fs';
-import { ConfigOptions, IConfig, parseOptions } from '../config/config';
+import { IConfig } from '../config/config';
+import { overwrite_settings, read_settings } from '../plugin/handlers';
+import { invoke } from '@tauri-apps/api';
 
 /**
  * @internal
  */
 export async function saveSettings
   <SettingsSchema extends any>
-  (newSettings: SettingsSchema, path: string, config: IConfig)
-{
+  (newSettings: SettingsSchema, path: string, config: IConfig): Promise<void> {
   try {
-    return await writeFile({
-      contents: JSON.stringify(newSettings, null, config.prettify ? config.numSpaces : 0),
-      path
-    })
+    if (config.usePlugin) {
+      await overwrite_settings(newSettings);
+    }
+    else {
+      await writeFile({
+        contents: JSON.stringify(newSettings, null, config.prettify ? config.numSpaces : 0),
+        path
+      })
+    }
   }
   catch (e) {
     throw e;
@@ -27,15 +33,25 @@ export async function saveSettings
  */
 export async function getSettings
   <SettingsSchema extends any>
-  (config: IConfig): Promise<{ settings: SettingsSchema, path: string, status: STATUS }>
-{
+  (config: IConfig): Promise<{ settings: SettingsSchema, path: string, status: STATUS }> {
   try {
-    const settingsFile = await ensureSettingsFile(config);
+    if (config.usePlugin) {
+      let [settings, path, was_created] = await read_settings();
 
-    return {
-      settings: JSON.parse(settingsFile.content) as SettingsSchema,
-      path: settingsFile.path,
-      status: settingsFile.status
+      return {
+        settings,
+        path: path,
+        status: was_created ? STATUS.FILE_CREATED : STATUS.FILE_EXISTS,
+      }
+    }
+    else {
+      const settingsFile = await ensureSettingsFile(config);
+
+      return {
+        settings: JSON.parse(settingsFile.content) as SettingsSchema,
+        path: settingsFile.path,
+        status: settingsFile.status
+      }
     }
   }
   catch (e) {
