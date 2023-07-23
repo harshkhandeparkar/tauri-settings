0ad0052e830981b79dc4c4ee90b6e6c0ff1311c7
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-23 15:33:57 +0530

    feat: run rustfmt

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,23 +1,29 @@
 use config::Config;
 use tauri::{
-	plugin::{Builder, TauriPlugin}, Manager, Runtime
+    plugin::{Builder, TauriPlugin},
+    Manager, Runtime,
 };
 
 mod config;
+mod dot_notation;
 mod fs;
 mod handlers;
-mod dot_notation;
 #[cfg(test)]
 mod test;
 
 pub fn init<R: Runtime>(config: Option<Config>) -> TauriPlugin<R> {
-	Builder::new("settings")
-		.invoke_handler(tauri::generate_handler![handlers::has, handlers::get, handlers::set])
-		.setup(|app| {
-			let plugin_state = config.unwrap_or(Config::new(&app.config(), None, None, None, None)?);
+    Builder::new("settings")
+        .invoke_handler(tauri::generate_handler![
+            handlers::has,
+            handlers::get,
+            handlers::set
+        ])
+        .setup(|app| {
+            let plugin_state =
+                config.unwrap_or(Config::new(&app.config(), None, None, None, None)?);
 
-			app.manage(plugin_state);
-			Ok(())
-		})
-		.build()
+            app.manage(plugin_state);
+            Ok(())
+        })
+        .build()
 }
--- plugin/src/test.rs
+++ plugin/src/test.rs
@@ -1,8 +1,9 @@
-use serde_json::Value;
 use crate::dot_notation::{get_dot_notation, set_dot_notation};
+use serde_json::Value;
 
 fn get_dummy_data() -> Value {
-	serde_json::from_str(r#"
+    serde_json::from_str(
+        r#"
 	{
 		"name": "John Doe",
 		"age": 43,
@@ -10,11 +11,14 @@ fn get_dummy_data() -> Value {
 			"theme": "dark",
 			"open_on_start": true
 		}
-	}"#).unwrap()
+	}"#,
+    )
+    .unwrap()
 }
 
 fn get_updated_dummy_data() -> Value {
-	serde_json::from_str(r#"
+    serde_json::from_str(
+        r#"
 	{
 		"name": "John Lark",
 		"age": 40,
@@ -30,40 +34,49 @@ fn get_updated_dummy_data() -> Value {
 		"test": {
 			"test_path": "ok"
 		}
-	}"#).unwrap()
+	}"#,
+    )
+    .unwrap()
 }
 
 #[test]
 fn get_dot_notation_works() {
-	let data: Value = get_dummy_data();
+    let data: Value = get_dummy_data();
 
-	let get_name = get_dot_notation(&data, "name".into());
-	let get_age = get_dot_notation(&data, "age".into());
-	let get_theme = get_dot_notation(&data, "preferences.theme".into());
-	let get_startup = get_dot_notation(&data, "preferences.open_on_start".into());
-	let get_null = get_dot_notation(&data, "preferences.fullscreen".into());
+    let get_name = get_dot_notation(&data, "name".into());
+    let get_age = get_dot_notation(&data, "age".into());
+    let get_theme = get_dot_notation(&data, "preferences.theme".into());
+    let get_startup = get_dot_notation(&data, "preferences.open_on_start".into());
+    let get_null = get_dot_notation(&data, "preferences.fullscreen".into());
 
-	assert_eq!(get_name, "John Doe");
-	assert_eq!(get_age, 43);
-	assert_eq!(get_theme, "dark");
-	assert_eq!(get_startup, true);
-	assert_eq!(get_null, Value::Null);
+    assert_eq!(get_name, "John Doe");
+    assert_eq!(get_age, 43);
+    assert_eq!(get_theme, "dark");
+    assert_eq!(get_startup, true);
+    assert_eq!(get_null, Value::Null);
 }
 
 #[test]
 fn set_dot_notation_works() {
-	let data: Value = get_dummy_data();
-	let updated_data: Value = get_updated_dummy_data();
+    let data: Value = get_dummy_data();
+    let updated_data: Value = get_updated_dummy_data();
 
-	let data = set_dot_notation(data, "name".into(), "John Lark".into());
-	let data = set_dot_notation(data, "age".into(), 40.into());
-	let data = set_dot_notation(data, "preferences.theme".into(), "light".into());
-	let data = set_dot_notation(data, "preferences.open_on_start".into(), 0.into());
-	let data = set_dot_notation(data, "preferences.fullscreen".into(), true.into());
-	let data = set_dot_notation(data, "preferences.run_out_of_names.another_setting".into(), 12.into());
-	let data = set_dot_notation(data, "recently_opened".into(), vec!["file1", "file2"].into());
-	let data = set_dot_notation(data, "test.test_path".into(), "ok".into());
+    let data = set_dot_notation(data, "name".into(), "John Lark".into());
+    let data = set_dot_notation(data, "age".into(), 40.into());
+    let data = set_dot_notation(data, "preferences.theme".into(), "light".into());
+    let data = set_dot_notation(data, "preferences.open_on_start".into(), 0.into());
+    let data = set_dot_notation(data, "preferences.fullscreen".into(), true.into());
+    let data = set_dot_notation(
+        data,
+        "preferences.run_out_of_names.another_setting".into(),
+        12.into(),
+    );
+    let data = set_dot_notation(
+        data,
+        "recently_opened".into(),
+        vec!["file1", "file2"].into(),
+    );
+    let data = set_dot_notation(data, "test.test_path".into(), "ok".into());
 
-
-	assert_eq!(data, updated_data);
+    assert_eq!(data, updated_data);
 }
