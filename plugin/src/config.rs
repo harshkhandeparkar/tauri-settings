2416e9487be307d2a8b02f9045afb3402cdb216f
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-25 13:31:20 +0530

    feat: added config options

--- plugin/src/config.rs
+++ plugin/src/config.rs
@@ -1,11 +1,20 @@
+use std::error::Error;
 use tauri::api::path;
 
-#[derive(Debug, Default, serde::Deserialize, Clone)]
+#[derive(Debug, Default, Clone)]
 pub struct Config {
 	pub file_name: String,
 	pub directory: String,
 	pub prettify: bool,
 	pub num_spaces: u8,
+}
+
+#[derive(Debug, Default, serde::Deserialize, Clone)]
+pub struct ConfigOptions {
+	pub file_name: Option<String>,
+	pub directory: Option<String>,
+	pub prettify: Option<bool>,
+	pub num_spaces: Option<u8>,
 }
 
 impl Config {
@@ -15,7 +24,7 @@ impl Config {
 		directory: Option<String>,
 		prettify: Option<bool>,
 		num_spaces: Option<u8>,
-	) -> Result<Config, Box<dyn std::error::Error>> {
+	) -> Result<Config, Box<dyn Error>> {
 		let config_directory: String = directory.unwrap_or(
 			path::app_config_dir(app_config)
 				.ok_or("Error: Default config directory not found.")?
@@ -30,5 +39,18 @@ impl Config {
 			prettify: prettify.unwrap_or(false),
 			num_spaces: num_spaces.unwrap_or(0),
 		})
+	}
+
+	pub fn from_config_options(
+		app_config: &tauri::Config,
+		options: &ConfigOptions,
+	) -> Result<Config, Box<dyn Error>> {
+		Config::new(
+			app_config,
+			options.file_name.clone(),
+			options.directory.clone(),
+			options.prettify,
+			options.num_spaces,
+		)
 	}
 }
--- plugin/src/handlers.rs
+++ plugin/src/handlers.rs
@@ -2,53 +2,69 @@ use serde_json::Value;
 use tauri::{AppHandle, Runtime, State};
 
 use crate::{
-	config::Config,
+	config::{Config, ConfigOptions},
 	fs::{load_settings_json, save_settings_json},
 	settings,
 };
 
 #[tauri::command]
 pub fn has<R: Runtime>(
-	_app: AppHandle<R>,
+	app: AppHandle<R>,
 	state: State<'_, Config>,
 	key: &str,
-	custom_config: Option<Config>,
+	custom_config: Option<ConfigOptions>,
 ) -> Result<bool, String> {
-	let config = &custom_config.unwrap_or_else(|| state.inner().clone());
+	let config = &custom_config
+		.map(|options| Config::from_config_options(&app.config(), &options))
+		.unwrap_or_else(|| Ok(state.inner().clone()))
+		.map_err(|err| err.to_string())?;
+
 	settings::has(config, key).map_err(|err| err.to_string())
 }
 
 #[tauri::command]
 pub fn get<R: Runtime>(
-	_app: AppHandle<R>,
+	app: AppHandle<R>,
 	state: State<'_, Config>,
 	key: &str,
-	custom_config: Option<Config>,
+	custom_config: Option<ConfigOptions>,
 ) -> Result<Value, String> {
-	let config = &custom_config.unwrap_or_else(|| state.inner().clone());
+	let config = &custom_config
+		.map(|options| Config::from_config_options(&app.config(), &options))
+		.unwrap_or_else(|| Ok(state.inner().clone()))
+		.map_err(|err| err.to_string())?;
+
 	settings::get(config, key).map_err(|err| err.to_string())
 }
 
 #[tauri::command]
 pub fn set<R: Runtime>(
-	_app: AppHandle<R>,
+	app: AppHandle<R>,
 	state: State<'_, Config>,
 	key: &str,
 	value: Value,
-	custom_config: Option<Config>,
+	custom_config: Option<ConfigOptions>,
 ) -> Result<Value, String> {
-	let config = &custom_config.unwrap_or_else(|| state.inner().clone());
+	let config = &custom_config
+		.map(|options| Config::from_config_options(&app.config(), &options))
+		.unwrap_or_else(|| Ok(state.inner().clone()))
+		.map_err(|err| err.to_string())?;
+
 	settings::set(config, key, value).map_err(|err| err.to_string())
 }
 
 #[tauri::command]
 pub fn overwrite_settings<R: Runtime>(
-	_app: AppHandle<R>,
+	app: AppHandle<R>,
 	state: State<'_, Config>,
 	new_settings: Value,
-	custom_config: Option<Config>,
+	custom_config: Option<ConfigOptions>,
 ) -> Result<(), String> {
-	let config = &custom_config.unwrap_or_else(|| state.inner().clone());
+	let config = &custom_config
+		.map(|options| Config::from_config_options(&app.config(), &options))
+		.unwrap_or_else(|| Ok(state.inner().clone()))
+		.map_err(|err| err.to_string())?;
+
 	save_settings_json(&new_settings, &config).map_err(|err| err.to_string())?;
 
 	Ok(())
@@ -56,11 +72,15 @@ pub fn overwrite_settings<R: Runtime>(
 
 #[tauri::command]
 pub fn read_settings<R: Runtime>(
-	_app: AppHandle<R>,
+	app: AppHandle<R>,
 	state: State<'_, Config>,
-	custom_config: Option<Config>,
+	custom_config: Option<ConfigOptions>,
 ) -> Result<(Value, String, bool), String> {
-	let config = &custom_config.unwrap_or_else(|| state.inner().clone());
+	let config = &custom_config
+		.map(|options| Config::from_config_options(&app.config(), &options))
+		.unwrap_or_else(|| Ok(state.inner().clone()))
+		.map_err(|err| err.to_string())?;
+
 	let (settings_json, settings_file_path, was_created) =
 		load_settings_json(config).map_err(|err| err.to_string())?;
 
