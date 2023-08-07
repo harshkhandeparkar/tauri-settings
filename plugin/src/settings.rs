7de5250e474943bd8ebbea41da2ce36c8ac2f9a0
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-08-07 19:13:02 +0530

    yeet: SettingsManager

--- plugin/src/settings.rs
+++ plugin/src/settings.rs
@@ -9,7 +9,7 @@ use crate::{
 	fs::{load_settings_json, save_settings_json},
 };
 
-pub trait SettingsSchema: Sized + Serialize + DeserializeOwned + Default + Copy {}
+pub trait SettingsSchema: Serialize + DeserializeOwned + Default + Clone {}
 
 /// Checks if a key exists in the settings.
 ///
@@ -75,86 +75,4 @@ pub fn set<V: Serialize>(
 	save_settings_json(&new_settings, config)?;
 
 	Ok(new_settings)
-}
-
-///
-pub struct SettingsManager<S: SettingsSchema> {
-	settings: S,
-	config: Config,
-}
-
-impl<S: SettingsSchema> SettingsManager<S> {
-	pub fn new(
-		app_config: &tauri::Config,
-		file_name: Option<String>,
-		directory: Option<String>,
-		prettify: Option<bool>,
-	) -> Result<SettingsManager<S>, Box<dyn Error>> {
-		let config = Config::new(app_config, file_name, directory, prettify)?;
-
-		Ok(SettingsManager {
-			settings: S::default(),
-			config,
-		})
-	}
-
-	pub fn initialize(&mut self) -> Result<(), Box<dyn Error>> {
-		let (current_settings_json, _, was_created) = load_settings_json(&self.config)?;
-
-		if was_created {
-			save_settings_json(&self.settings, &self.config)?;
-		} else {
-			let new_settings: Value = serde_json::from_str(&current_settings_json)?;
-
-			self.settings = from_value(new_settings)?;
-		}
-
-		Ok(())
-	}
-
-	pub fn has_cache<T: SettingsSchema>(&self, key: &str) -> Result<bool, Box<dyn Error>> {
-		let value: T = self.get_cache(key)?;
-		let value = to_value(value)?;
-
-		Ok(!value.is_null())
-	}
-
-	pub fn get_cache<T: SettingsSchema>(&self, key: &str) -> Result<T, Box<dyn Error>> {
-		get_dot_notation(&self.settings, key)
-	}
-
-	pub fn set_cache<V: Serialize>(
-		&mut self,
-		key: &str,
-		new_value: V,
-	) -> Result<S, Box<dyn Error>> {
-		let new_settings: S = set_dot_notation(&self.settings, key, new_value)?;
-
-		self.settings = new_settings;
-		Ok(new_settings)
-	}
-
-	pub fn get<V: Serialize + DeserializeOwned + Clone>(
-		&mut self,
-		key: &str,
-	) -> Result<V, Box<dyn Error>> {
-		let value: V = get(&self.config, key)?;
-
-		self.set_cache(key, value.clone())?;
-
-		Ok(value)
-	}
-
-	pub fn set<V: Serialize>(&mut self, key: &str, new_value: V) -> Result<S, Box<dyn Error>> {
-		let new_settings: S = from_value(set(&self.config, key, new_value)?)?;
-		self.settings = new_settings;
-
-		Ok(new_settings)
-	}
-
-	pub fn sync_cache(&self) -> Result<S, Box<dyn Error>> {
-		save_settings_json(&self.settings, &self.config)?;
-
-		Ok(self.settings)
-	}
 }
