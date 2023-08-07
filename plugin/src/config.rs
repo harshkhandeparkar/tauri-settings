82ec61e8723d4fd03de2f2652cd937ded2bd41df
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-08-07 22:27:54 +0530

    docs: completed documentation for config module

--- plugin/src/config.rs
+++ plugin/src/config.rs
@@ -52,6 +52,12 @@ impl ConfigOptions {
 impl Config {
 	/// Creates a new Config struct.
 	///
+	/// ### Arguments
+	/// * `app_config`: The tauri app config.
+	/// * `file_name`: The name of the JSON file (with the extension) where the settings will be stored. (Default: `settings.json`)
+	/// * `directory`: Path to the directory where the settings file will be stored. (Default: [App config directory](https://tauri.app/v1/api/js/path#appconfigdir))
+	/// * `prettify`: Whether to prettify the JSON settings data. (Default: `false`)
+	///
 	/// ### Examples
 	/// ```no_run
 	/// # use tauri_plugin_settings::Config;
@@ -85,6 +91,18 @@ impl Config {
 		})
 	}
 
+	/// Creates a new Config from a ConfigOptions struct.
+	///
+	/// Defaults: See [`Config::new`].
+	/// ### Examples
+	/// ```no_run
+	/// # use tauri_plugin_settings::{Config, ConfigOptions};
+	/// # let app_config = tauri::Config::default();
+	/// // Where app_config is tauri::Config
+	///
+	/// let config_options = ConfigOptions::new(Some("preferences.json".into()), None, Some(true));
+	/// let config = Config::from_config_options(&app_config, &config_options);
+	/// ```
 	pub fn from_config_options(
 		app_config: &tauri::Config,
 		options: &ConfigOptions,
@@ -97,6 +115,19 @@ impl Config {
 		)
 	}
 
+	/// Creates a new Config with the default values.
+	///
+	/// All `None` fields are replaced by their default values.
+	///
+	/// Default: See [`Config::new`].
+	///
+	/// ### Examples
+	/// ```no_run
+	/// # use tauri_plugin_settings::Config;
+	/// # let app_config = tauri::Config::default();
+	/// // Where app_config is tauri::Config
+	/// let config = Config::default(&app_config);
+	/// ```
 	pub fn default(app_config: &tauri::Config) -> Result<Config, Box<dyn Error>> {
 		Config::new(app_config, None, None, None)
 	}
