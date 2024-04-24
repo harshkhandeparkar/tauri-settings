adb2621cd321109281b1e69c51d9584655e7a7f7
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-04-24 15:57:49 +0530

    feat: added an option to disable file addition from frontend

--- plugin/src/config.rs
+++ plugin/src/config.rs
@@ -8,19 +8,23 @@ use tauri::api::path;
 pub struct PluginConfig {
 	pub scope: PathBuf,
 	pub files_limit: u32,
+	pub allow_file_addition: bool,
 }
 
 #[derive(Debug, Clone)]
 pub struct PluginConfigOptions {
 	pub scope: Option<PathBuf>,
 	pub files_limit: Option<u32>,
+	pub allow_file_addition: Option<bool>,
 }
 
 impl PluginConfig {
 	pub fn new(
 		app_config: &tauri::Config,
 		scope: Option<PathBuf>,
-		files_limit: Option<u32>
+		files_limit: Option<u32>,
+		allow_file_addition: Option<bool>,
+
 	) -> Result<Self, Box<dyn Error>> {
 		let scope = scope.unwrap_or(
 			path::app_config_dir(app_config).ok_or("Error: Default config directory not found.")?,
@@ -29,6 +33,7 @@ impl PluginConfig {
 		Ok(Self {
 			scope,
 			files_limit: files_limit.unwrap_or(0),
+			allow_file_addition: allow_file_addition.unwrap_or(false)
 		})
 	}
 
@@ -39,7 +44,8 @@ impl PluginConfig {
 		Self::new(
 			app_config,
 			options.scope.clone(),
-			options.files_limit
+			options.files_limit,
+			options.allow_file_addition
 		)
 	}
 }
