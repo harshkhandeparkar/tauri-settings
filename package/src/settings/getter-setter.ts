108475e30b0f7a39a87ba3effcd89c77b2e57ad7
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-04-30 00:31:04 +0530

    feat: support scoped path in getter setter

--- package/src/settings/getter-setter.ts
+++ package/src/settings/getter-setter.ts
@@ -1,15 +1,25 @@
 import type { Path, PathValue } from '../utils/dot-notation';
-import { has as invokeHas, get as invokeGet, set as invokeSet } from '../utils/handlers';
+import { has as invokeHas, get as invokeGet, set as invokeSet, get_settings_file_id } from '../utils/handlers';
 
 /**
  * Checks whether a key exists in the settings.
- * @param key The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation.
  */
 export async function has<
 	SettingsSchema = {},
 	K extends Path<SettingsSchema> = Path<SettingsSchema>
-	>(key: K, fileId?: number): Promise<boolean> {
+	>(
+		/** The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation. */
+		key: K,
+		/** The settings file to search. Can search using the ID of the settings file or by a path (string) relative to the scope directory. */
+		file?: number | string
+	): Promise<boolean> {
 	try {
+		const fileId = typeof file === 'string' ? await get_settings_file_id(file) : file;
+
+		if (fileId === null) {
+			throw new Error(`Settings file \`${file}\` not found.`);
+		}
+
 		return await invokeHas(key as string, fileId);
 	}
 	catch (e) {
@@ -20,14 +30,23 @@ export async function has<
 
 /**
  * Get the value of a particular setting.
- * @param key The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation.
- * @returns The value of the setting
  */
 export async function get<
 	SettingsSchema = {},
 	K extends Path<SettingsSchema> = Path<SettingsSchema>
-	>(key: K, fileId?: number): Promise<PathValue<SettingsSchema, K>> {
+	>(
+		/** The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation. */
+		key: K,
+		/** The settings file to search. Can search using the ID of the settings file or by a path (string) relative to the scope directory. */
+		file?: number | string
+	): Promise<PathValue<SettingsSchema, K>> {
 	try {
+		const fileId = typeof file === 'string' ? await get_settings_file_id(file) : file;
+
+		if (fileId === null) {
+			throw new Error(`Settings file \`${file}\` not found.`);
+		}
+
 		return await invokeGet(key as string, fileId);
 	}
 	catch (e) {
@@ -37,16 +56,26 @@ export async function get<
 
 /**
  * Sets the value of a particular setting.
- * @param key The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation.
- * @param value The new value
- * @returns The entire settings object
  */
 export async function set<
 	SettingsSchema = {},
 	K extends Path<SettingsSchema> = Path<SettingsSchema>,
 	V extends PathValue<SettingsSchema, K> = PathValue<SettingsSchema, K>
-	>(key: K, value: V, fileId?: number): Promise<void> {
+	>(
+		/** The key for the setting. Key supports dot notation. See https://github.com/harshkhandeparkar/tauri-settings#dot-notation. */
+		key: K,
+		/** The new value to set. */
+		value: V,
+		/** The settings file to search. Can search using the ID of the settings file or by a path (string) relative to the scope directory. */
+		file?: number | string
+	): Promise<void> {
 	try {
+		const fileId = typeof file === 'string' ? await get_settings_file_id(file) : file;
+
+		if (fileId === null) {
+			throw new Error(`Settings file \`${file}\` not found.`);
+		}
+
 		await invokeSet(key as string, value, fileId);
 	}
 	catch (e) {
