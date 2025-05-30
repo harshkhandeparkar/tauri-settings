941f6d2038929ca363248518b37fc7cfcf28cc32
superboxes <red.reduose@gmail.com>
2025-05-30 18:46:52 +0300

    updated to tauri v2

--- src/fs/load-save.ts
+++ src/fs/load-save.ts
@@ -1,46 +1,47 @@
-import { ensureSettingsFile, STATUS } from './ensure-settings-file';
-import { writeFile } from '@tauri-apps/api/fs';
-import { ConfigOptions, parseOptions } from '../config/config';
+import { ensureSettingsFile, STATUS } from "./ensure-settings-file";
+import { writeFile } from "@tauri-apps/plugin-fs";
+import { ConfigOptions, parseOptions } from "../config/config";
 
 /**
  * @internal
  */
-export async function saveSettings
-  <SettingsSchema extends any>
-  (newSettings: SettingsSchema, path: string, options: ConfigOptions)
-{
+export async function saveSettings<SettingsSchema extends any>(
+  newSettings: SettingsSchema,
+  path: string,
+  options: ConfigOptions,
+) {
   try {
     const finalConfig = parseOptions(options);
 
-    return await writeFile({
-      contents: JSON.stringify(newSettings, null, finalConfig.prettify ? finalConfig.numSpaces : 0),
-      path
-    })
-  }
-  catch (e) {
+    return await writeFile(
+      path,
+      JSON.stringify(
+        newSettings,
+        null,
+        finalConfig.prettify ? finalConfig.numSpaces : 0,
+      ),
+    );
+  } catch (e) {
     throw e;
   }
 }
-
 
 /**
  * Get all the settings.
  * @returns The entire settings object.
  */
-export async function getSettings
-  <SettingsSchema extends any>
-  (options: ConfigOptions = {}): Promise<{settings: SettingsSchema, path: string, status: STATUS}>
-{
+export async function getSettings<SettingsSchema extends any>(
+  options: ConfigOptions = {},
+): Promise<{ settings: SettingsSchema; path: string; status: STATUS }> {
   try {
     const settingsFile = await ensureSettingsFile(options);
 
     return {
       settings: JSON.parse(settingsFile.content) as SettingsSchema,
       path: settingsFile.path,
-      status: settingsFile.status
-    }
-  }
-  catch (e) {
+      status: settingsFile.status,
+    };
+  } catch (e) {
     throw e;
   }
 }
